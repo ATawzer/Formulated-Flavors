46aece0e0d3e9e3812bff17cfe4e88d0fe343578
Tawzer <Arlin.Tawzer@CenturyLink.com>
2020-11-11 10:09:50 -0700

    Import cleanup in scripts

--- scripts/db_funcs.py
+++ scripts/db_funcs.py
@@ -1,8 +1,4 @@
 import pandas as pd
-from recipe_scrapers import scrape_me
-import time
-import random
-import json
 import pymongo
 from tqdm.notebook import tqdm
 import numpy as np
--- scripts/parsers.py
+++ scripts/parsers.py
@@ -1,11 +1,3 @@
-import pandas as pd
-from recipe_scrapers import scrape_me
-import time
-import random
-import json
-import pymongo
-from tqdm.notebook import tqdm
-import numpy as np
 import ast
 
 class IngredientParser:
